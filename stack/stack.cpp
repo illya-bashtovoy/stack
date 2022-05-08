e72f502b62af5e6291535005a481ac664bee432d
prefa <prefa@DESKTOP-1A7AP5A>
2022-05-08 17:47:57 +0300

    Добавьте файлы проекта.

---
+++ stack/stack.cpp
@@ -0,0 +1,30 @@
+﻿/*
+1. Доопрацюйте клас Stack. Зборіть так, щоб клас Stack був шаблонним.
+*/
+
+#include <iostream>
+#include "Stack.h"
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Stack <int> A(3);
+
+	cout << "Iндекс < макс =" << A.push(45) << endl; 
+
+	cout << "Iндекс < макс =" << A.push(67) << endl;
+
+	cout << "Iндекс < макс =" << A.push(125) << endl;
+
+	cout << "Стек заповнений =" << A.isFull() << endl;
+
+	for (int i = 0; i < 3; i++) {
+		std::cout << A.pop() << "\t";
+	}
+
+	cout << endl << "Стек порожній =" << A.isEmpty() << endl;	 
+}
---
+++ stack/stack.h
@@ -0,0 +1,72 @@
+﻿/*
+1. Доопрацюйте клас Stack. Зборіть так, щоб клас Stack був шаблонним.
+*/
+
+
+#pragma once
+#include <iostream>
+template <typename T>
+class Stack {
+private:
+	T* arr;
+	T max;
+	T index;
+public:
+	Stack() {}
+	Stack(T);
+	~Stack();
+	bool push(T n);
+	T pop();
+	bool isEmpty();
+	bool isFull();
+};
+
+
+template <typename T>
+Stack<T>::Stack(T size) {
+	max = 1000;
+	index = 0;
+	if (size > max) {
+		std::cout << "size > maxSize" << std::endl;
+	}
+	else {
+		this->max = size;
+		this->arr = new T[size];
+	}
+}
+
+
+template <typename T>
+Stack<T>::~Stack() {
+	delete[] arr;
+}
+
+
+template <typename T>
+bool Stack<T>::push(T n) {
+	if (index >= max) return false;
+	else {
+		arr[index] = n;
+		index++;
+		return true;
+	}
+}
+
+
+template <typename T>
+T Stack<T>::pop() {
+	if (index < 0) return -1;
+	else return arr[--index];
+}
+
+
+template <typename T>
+bool Stack<T>::isEmpty() {
+	return index <= 0;
+}
+
+
+template <typename T>
+bool Stack<T>::isFull() {
+	return index == max;
+}
